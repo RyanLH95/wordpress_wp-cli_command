c02ed0e17d44d24f3686c41a5b8ded46be3aeee2
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 21:48:02 +0100

    Create a public function query for WP_Query Posts by creating an array with post with 4 arguments and a query variable to equal new WP_Query

--- wpc-cli/wp-cli_command.php
+++ wpc-cli/wp-cli_command.php
@@ -10,11 +10,30 @@
 
 if (defined('WP_CLI') && WP_CLI) {
 
- require_once plugin_dir_path(__FILE__) . '/vendor/fakerphp/faker/src/
- autoload.php';
+  require_once plugin_dir_path(__FILE__) . '/vendor/fakerphp/faker/src/
+  autoload.php';
 
- WP_CLI::add_command();
+  class WPC_CLI {
 
+    public function custom_wp_query( $args, $assoc_args ) {
+        $query_args = array(
+            'post_type'      => 'post',
+        
+            // Query performance optimization.
+            'fields'         => 'ids',
+            'no_found_rows'  => true,
+            'posts_per_page' => -1,
+        );
+        
+        $query = new WP_Query( $query_args );
+        if ( $query->posts ) {
+            foreach ( $query->posts as $key => $post_id ) {
+                WP_CLI::line( $post_id );
+            }
+        }
 
+  WP_CLI::add_command('wpc', 'WPC_CLI');
+    }
+  }
  // up wpc generate_floorplans --amount=10
 }
