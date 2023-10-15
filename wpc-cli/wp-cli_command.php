8c1f876c44a2c0887b35cfadbed72950247306f7
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 19:33:05 +0100

    Create directory of wpc-cli and install wp-cli and composer, the write a function in wp-cli_command.php to pPATH

---
+++ wpc-cli/wp-cli_command.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP-CLI 'dmg-read-more-search'
+ * Plugin URI: N/A
+ * Description: Custom WP-CLI Commands
+ * Authour: Ryan Henry
+ * Version: 1.0.0
+ * Author URI: https://ryanhenry.co.uk
+ */
+
+ require_once plugin_dir_path(__FILE__) . '/vendor/fakerphp/faker/src/
+ autoload.php';
